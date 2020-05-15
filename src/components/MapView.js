834df681dd0f6ba8c65313bfbe027d1e7f6b9dd0
Qiacheng Li <choliaky@gmail.com>
2020-05-15 12:10:15 -0700

    added a feature to locate current location

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,43 +1,78 @@
 import React, {Component} from 'react'
-import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 
-const getUserPosition = () =>navigator.geolocation.getCurrentPosition(position => {const { latitude, longitude } = position.coords
-    })
 
-    class MapView extends Component{
+class MapView extends Component{
   constructor(props){
     super(props)
 
+    this.state ={
+      currentLocation: {lat: 45.51, lng:-122.68},
+      zoom: 13,
+      userLocation:[],
+      userLocationFound: false,
+    }
+  this.getUserPosition = this.getUserPosition.bind(this)
+  }
   
 
-  this.state ={
-    currentLocation: {lat: 45.51, lng:-122.68},
-    zoom: 13,
-    getUserPosition,
+  getUserPosition(){
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ userLocation : [position.coords.latitude, position.coords.longitude], userLocationFound:true, currentLocation : [position.coords.latitude, position.coords.longitude]})
+      
+      console.log(this.state)
+      
+    })
+  
+  }
+
+  componentDidMount(){
+    this.getUserPosition()
+  }
+
+  handleLocationFound = (e) => {
+    this.setState({
+      userLocationFound: true,
+      userLocation: e.latlng,
+    })
+  }
+
+  handleClick = () => {
+    const map = this.mapRef.current
+    if (map != null) {
+      map.leafletElement.locate()
+    }
   }
 
 
-}
+  render(){
+    const userLocation = this.state.userLocationFound ? (
+      <Marker position={this.state.userLocation}>
+        <Popup>Your location</Popup>
+      </Marker>
+    ) : null
 
-
-render(){
-const{currentLocation, zoom,getUserPosition}= this.state;
-
-return (
-  <Map center={currentLocation} zoom={zoom}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Marker position={currentLocation}>
-      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
-  </Map>
-);
-}
-
+    return (
+      <Map center={this.state.currentLocation} zoom={13} maxZoom={18} >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        {userLocation}     
+      </Map>
+    );
+    }
 }
 
 export default MapView;
