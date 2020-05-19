1f6ce919f97455ebb6c7958876f4ac65d8e5c23d
Geoffrey <geoolson@pdx.edu>
2020-05-18 20:32:17 -0700

    added a router and boostrap
    There's a little navbar at the top dmeonstrating the router and
    bootstrap styling.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import './App.css';
 import MapView from './components/MapView.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <div className="">
+        <nav className="bg-dark" style={{ zIndex: 1500 }}>
+          <ul className="list-inline text-center" style={{ marginBottom: 0 }}>
+            <li className="list-inline-item">
+              <Link className="text-light" to="/">Home</Link>
+            </li>
+            <li className="list-inline-item">
+              <Link className="text-light" to="/admin">Admin</Link>
+            </li>
+            <li className="list-inline-item">
+              <Link className="text-light" to="/student">Student</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/student">
+            <Student />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => {
+  return (
     <div className="App">
-     <MapView/>
+      <MapView />
     </div>
   );
+}
+
+const Admin = () => {
+  return <h2>Admin</h2>;
+}
+
+const Student = () => {
+  return <h2>Student</h2>;
 }
 
 export default App;
