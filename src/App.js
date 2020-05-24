1161cea41e46fc6e33379d2d41aed1467ba52143
qiacheng <choliaky@gmail.com>
2020-05-24 16:10:56 -0700

    added hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import MapView from './components/MapView.js'
-import Menu from './components/Menu.js'
+import Menu from './components/Menu/Menu.js'
 import { Stitch , ObjectID} from 'mongodb-stitch-browser-sdk'
 import Login from './components/Login'
 import DropPin from './components/DropPin';
-
+import NavMenu from './components/Menu/NavMenu'
 
 
 import {
@@ -15,6 +16,7 @@ import {
   HashRouter,
   Link
 } from "react-router-dom";
+import { thisExpression } from '@babel/types';
 
 
 export default class App extends Component {
@@ -25,19 +27,12 @@ export default class App extends Component {
     this.client = Stitch.hasAppClient(appId)
       ? Stitch.getAppClient(appId)
       : Stitch.initializeDefaultAppClient(appId)
+      
     if (this.client.auth.hasRedirectResult()) {
       console.log("has results")
 
       this.client.auth.handleRedirectResult().then(user => {
-        this.setState(
-          {
-            username: this.client.auth.authInfo.userProfile.data.name,
-            useremail: this.client.auth.authInfo.userProfile.data.email,
-            userID: this.client.auth.authInfo.userId,
-
-          }
-        )
-        window.location.replace('#/')
+        window.location.reload()
       }
       )
     }
@@ -47,14 +42,24 @@ export default class App extends Component {
 
     }
     this.state = {
-
+      isLoggedIn : false,
     }
 
+    this.NavMenu = this.NavMenu.bind(this)
   }
 
   componentDidMount() {
 
+  }
 
+  handleStitchClient(){
+    this.setState(
+      {
+        stitchClient : this.client,
+        userProfile : this.client.auth.userProfile,
+        isLoggedIn : true,
+      }
+    )
 
   }
 
@@ -75,36 +80,28 @@ export default class App extends Component {
     }
   }
 
+  NavMenu = () => {
+    if (this.client.auth.isLoggedIn) {
+      return (
+        <NavMenu />
+      );
+    }
+  }
+
   
 
   render() {
     return (
       <HashRouter basename="/">
-        <div className="">
-          <nav className="bg-dark" style={{ zIndex: 1500 }}>
-            <ul className="list-inline text-center" style={{ marginBottom: 0 }}>
-              <li className="list-inline-item">
-                <Link className="text-light" to="/">Home</Link>
-              </li>
-              {/* <li className="list-inline-item">
-                <Link className="text-light" to="/admin">Admin</Link>
-              </li>
-              <li className="list-inline-item">
-                <Link className="text-light" to="/student">Student</Link>
-              </li> */}
-              <li className="list-inline-item">
-                <Link className="text-light" to="/menu">Menu</Link>
-              </li> 
-              <li className="list-inline-item">
-                <Link className="text-light" to="/droppin">Drop Pin</Link>
-              </li> 
-            </ul>
-          </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        <div className="">
+          <div>
+          {this.NavMenu()}
+          </div>
+
+          <div>
           <Switch>
-            <Route exact path="/" component={this.Home} />
+            <Route exact path="/">{this.Home}</Route>
             <Route exact path="/menu" component={Menu} />
             <Route exact path="/droppin">
               <DropPin />
@@ -112,6 +109,7 @@ export default class App extends Component {
             <Route exact path="/create_module" component={Create_Modules} />
             <Route exact path="/view_module" component={View_Modules} />
           </Switch>
+          </div>
         </div>
       </HashRouter>
     );
---
+++ src/components/Menu/NavMenu.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components'
+import Menu from './Menu'
+import { is } from '@babel/types'
+
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 5%;
+  right: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 1502;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ open }) => open ? '#FFFFFF' : '#292b2c'};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => open ? '0' : '1'};
+      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    }
+  }
+`
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
+
+
+
+
+const NavMenu = () => {
+    const [open, setOpen] = React.useState(false);
+ 
+    return (
+        <div>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen}/>
+        </div>
+    )
+}
+
+export default NavMenu
