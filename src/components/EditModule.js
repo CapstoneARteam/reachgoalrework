5cc587001e941146168964ae1738587eb3061cee
Chriswpart <47011028+Chriswpart@users.noreply.github.com>
2020-06-03 15:37:16 -0700

    Update EditModule.js
    Saves to db now. Need to add save alert possibly.

--- src/components/EditModule.js
+++ src/components/EditModule.js
@@ -1,70 +1,107 @@
-import React from 'react'
+import React, { Component, } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import FormCheck from 'react-bootstrap/FormCheck'
 import FormControl from 'react-bootstrap/FormControl'
-import {Stitch, } from "mongodb-stitch-browser-sdk"
+import Alert from 'react-bootstrap/Alert'
+import {Stitch, RemoteMongoClient} from "mongodb-stitch-browser-sdk"
 
-const EditModule = () => {
-    return(
-        <Form style={{
-            top: '40px',
-            position: 'relative',
-            textAlign: 'center',
-        }}>
-            <Form.Group as={Row} controlId="formHorizontalName">
-                <Form.Label column sm={2}>
-                </Form.Label>
-                <Form.Control type="name" placeholder="Module Name" 
-                style={{ textAlign: 'center', width: '300px', margin: '0 auto',}} />
-            </Form.Group>
 
-            <Form.Group as={Row} controlId="formHorizontalTextarea1">
-                <Form.Label column sm={2}>
-                </Form.Label>
-                <Form.Control as="textarea" rows="5" placeholder="Description"
-                style={{ textAlign: 'center', width: '300px', margin: '0 auto',}}/>
-            </Form.Group>
-            <fieldset>
+
+
+export default class EditModule extends Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: true
+        }
+
+        const appId = "capstonear_app-xkqng"
+        this.client = Stitch.hasAppClient(appId)
+            ? Stitch.getAppClient(appId)
+            : Stitch.initializeDefaultAppClient(appId)
+        const mongodb = this.client.getServiceClient(
+            RemoteMongoClient.factory,
+            "mongodb-atlas"
+        )
+        this.db = mongodb.db("APP")
+    }
+
+
+
+    render(){
+        return(
+            <Form style={{
+                top: '40px',
+                position: 'relative',
+                textAlign: 'center',
+            }}>
+                <Form.Group>
+                    <Form.Control type="title" id="title" placeholder="Module Title" 
+                    style={{ textAlign: 'center', width: '300px', margin: '0 auto',}} />
+                </Form.Group>
+    
+                <Form.Group>
+                    <Form.Control as="textarea" rows="5" id="description" placeholder="Description"
+                    style={{ textAlign: 'center', width: '300px', margin: '0 auto',}}/>
+                </Form.Group>
+                <fieldset>
+                    <Form.Group as={Row}>
+                        <Col sm={10}>
+                            <Form.Check
+                            type="radio"
+                            label="Public"
+                            name="formHorizontalRadios"
+                            id="true"
+                            checked={this.state.selected === true} onChange={(e) => this.setState({ selected: true })}
+                            />
+                            <Form.Check
+                            type="radio"
+                            label="Private"
+                            name="formHorizontalRadios"
+                            id="false"
+                            checked={this.state.selected === false} onChange={(e) => this.setState({ selected: false })}
+                            />
+                        </Col>
+                    </Form.Group>
+                </fieldset>
+    
                 <Form.Group as={Row}>
-                    <Form.Label as="legend" column sm={2}>
-                    </Form.Label>
-                    <Col sm={10}>
-                        <Form.Check
-                        type="radio"
-                        label="Public"
-                        name="formHorizontalRadios"
-                        id="formHorizontalRadios1"
-                        />
-                        <Form.Check
-                        type="radio"
-                        label="Private"
-                        name="formHorizontalRadios"
-                        id="formHorizontalRadios2"
-                        />
+                    <Col sm={{ span: 10, offset: 2 }}>  
+                        <Button variant="primary">Add Pins</Button>
                     </Col>
                 </Form.Group>
-            </fieldset>
-
-            <Form.Group as={Row}>
-                <Col sm={{ span: 10, offset: 2 }}>  
-                    <Button variant="primary">Add Pins</Button>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row}>
-                <Col sm={{ span: 10, offset: 2 }}>
-                    <Button variant="primary">Save</Button>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row}>
-                <Col sm={{ span: 10, offset: 2 }}>   
-                    <Button variant="primary">Share</Button>
-                </Col>
-            </Form.Group>
-        </Form>
-    )
+                <Form.Group as={Row}>
+                    <Col sm={{ span: 10, offset: 2 }}>
+                        <Button className="btn btn-primary" onClick={() => {
+                            const title = document.getElementById("title").value || "";
+                            const description = document.getElementById("description").value || "";
+                            const radiotype = this.state.selected;
+                            this.db.collection("MODULES")
+                                .insertOne({
+                                    name: title,
+                                    owner_id: this.client.auth.authInfo.userId,
+                                    owner_email: this.state.useremail,
+                                    description: description,
+                                    pins: [],
+                                    shared_array: [],
+                                    public: radiotype,
+                                })
+                                .catch(console.error);
+                        }}>
+                            Save
+                        </Button>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Col sm={{ span: 10, offset: 2 }}>   
+                        <Button variant="primary">Share</Button>
+                    </Col>
+                </Form.Group>
+            </Form>
+        )
+    }
 }
-
-export default EditModule;
