174106807533a0c060ecddbd1b91be8a06bee812
Trung Nguyen <ntrung@pdx.edu>
2020-06-29 10:38:43 -0700

    Add finding pin from module

--- src/components/ViewPinOnMap.js
+++ src/components/ViewPinOnMap.js
@@ -16,7 +16,18 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
-
+const Style = {
+  position: "fixed",
+  width: "60px",
+  height: "60px",
+  bottom: "40px",
+  right: "40px",
+  backgroundColor: "#0C9",
+  color: "#FFF",
+  textAlign: "center",
+  boxShadow: "2px 2px 3px #999",
+  zIndex: 1500
+}
 var greenIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -46,7 +57,8 @@ class ViewPinOnMap extends Component{
       userID:"",
       stitch_res:[],
       pins:[],
-      pins_line:[]
+      pins_line:[],
+      pins_array:[],
     }
   this.getUserPosition = this.getUserPosition.bind(this)
   this.drawpins = this.drawpins.bind(this)
@@ -77,24 +89,30 @@ class ViewPinOnMap extends Component{
 
   componentDidMount(){
     this.getUserPosition()
+    this.drawpins()
   }
 
   drawpins() {
     if(!this.client.auth.isLoggedIn){
         return
     }
-    this.db.collection("PINS").find({_id: ObjectId("5ebed1bc5992681f357d7948")} )
+    this.db.collection("MODULES").find({_id: ObjectId("5efa19afa4cd11d00a3ed561")} )
     .asArray()
-    .then((pins) => {this.setState({pins})
-    //console.log(this.state.pins)
-    var temp_array =[]
-    for (var i =0; i< this.state.pins.length;++i)
-    {
-      temp_array.push([this.state.pins[i].lat,this.state.pins[i].long]);
-    }
-    //console.log(this.state.pins_line)
-    this.setState ({ pins_line: temp_array} ) 
-    }
+    .then((stitch_res) => {this.setState({stitch_res})
+      var temp_array =[]
+      for(var i=0; i< this.state.stitch_res[0].pins.length;i++)
+      {
+        this.db.collection("PINS").find({_id: ObjectId(this.state.stitch_res[0].pins[i])} )
+        .asArray()
+        .then((pins) => {this.setState({pins})
+          temp_array.push(this.state.pins);
+        })
+        this.setState ({ pins_array: temp_array} )
+      }
+     
+      //console.log(this.state.pins_array) 
+      //this.setState ({ pins_line: temp_array} ) 
+      }
     )
   }
   drawlines(){
@@ -120,33 +138,37 @@ class ViewPinOnMap extends Component{
   }
   render(){
     const userLocation = this.state.userLocationFound ? (
-      <Marker onClick={()=>this.openGoogle(this.state.userLocation[0],this.state.userLocation[1])} position={this.state.userLocation}  icon= {myIcon} >
+      <Marker  position={this.state.userLocation}  icon= {myIcon} >
         <Popup>Your location</Popup>
       </Marker>
     ) : null
    
-    
     return (
       <div>
-      {this.drawpins()}
       <Map center={this.state.currentLocation} zoom={13} maxZoom={18} >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        {userLocation} 
-        {this.state.pins.map((info,idx) => {
-            return <Marker onClick={()=>this.openGoogle(info.lat,info.long)} 
-                           key = {idx} position={[info.lat,info.long]} 
+        {userLocation}
+         
+        {this.state.pins_array.map((info,idx) => {
+            return <Marker 
+                           key = {idx} position={[info[0].lat,info[0].long]} 
                            icon= {new L.divIcon({
                                                   className: 'my-div-icon',
-                                                  html: '<span  class="my-div-span">'+(idx+1)+'</span>'+
+                                                  html: '<span style={Style} class="my-div-span">'+(idx+1)+'</span>'+
                                                         '<img class="my-div-image" src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"/>'
                                                 })} >
-                        <Popup>Pin location</Popup>
+                        <Popup>
+                              <h1>{info[0].desc}</h1>
+                              <p>{info.description}</p>
+                              <p>{info.hint}</p>
+                              <p>{info.destination}</p>
+                              <button onClick={()=>this.openGoogle(info.lat,info.long)} >Open Google Map</button>
+                        </Popup>
                     </Marker>
           })}
-          {this.drawlines()}
       </Map>
       </div>
     );
