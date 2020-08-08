def219986d5010ccac965d3d59b2e153d91f1013
Geoffrey <geoolson@pdx.edu>
2020-08-08 01:27:11 -0700

    quick correction

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import { Navbar } from "react-bootstrap";
 import ViewModules from "./components/ViewModules";
 import ManageModules from "./components/ManageModules";
 import ViewPinOnMap from "./components/ViewPinOnMap";
-import Completed from "./components/completed";
 
 import { Switch, Route, HashRouter } from "react-router-dom";
 
@@ -122,11 +121,6 @@ export default class App extends Component {
                             path="/module/:id/pins"
                             component={ViewPinOnMap}
                         />
-                        <Route
-                            path="/completed/:module/:userid"
-                        >
-                            <Completed/>
-                        </Route>
 
                         <Route
                             exact
